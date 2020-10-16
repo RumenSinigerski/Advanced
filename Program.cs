83dc6e9520dd198b01b911770f1ceebe570604bd
Rumen Sinigerski <69121051+RumenSinigerski@users.noreply.github.com>
2020-10-16 23:09:32 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace CarManufacturer
+namespace PokemonTrainer
 {
-    public class StartUp
+    public class Program
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split().ToArray();
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
 
-            string make = Console.ReadLine();
-            string model = Console.ReadLine();
-            int year = int.Parse(Console.ReadLine());
-            double fuelQuantity = double.Parse(Console.ReadLine());
-            double fuelConsumption = double.Parse(Console.ReadLine());
-            
+            while (input[0] != "Tournament")
+            {
 
-            Car firstCar = new Car();
-            Car secondCar = new Car(make, model, year);
-            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+                string trainerName = input[0];
+                string pokemonName = input[1];
+                string pokemonElement = input[2];
+                int pokemonHealth = int.Parse(input[3]);
 
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, new Trainer(trainerName, new List<Pokemon>()));
+                }
+
+                trainers[trainerName].Pokemons.Add(new Pokemon(pokemonName,pokemonElement, pokemonHealth));
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+
+            string element = Console.ReadLine();
+
+            while (element != "End")
+            {
+                foreach (var kvp in trainers)
+                {
+                    bool isTrue = kvp.Value.Pokemons.Any(p => p.Element == element);
+                    if (isTrue)
+                    {
+                        kvp.Value.Badges++;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < kvp.Value.Pokemons.Count; i++)
+                        {
+                            Pokemon pokemon = kvp.Value.Pokemons[i];
+                            if (pokemon.Health - 10 > 0)
+                            {
+                                pokemon.Health -= 10;
+                            }
+                            else
+                            {
+                                kvp.Value.Pokemons.Remove(pokemon);
+                                i--;
+                            }
+                        }
+                        
+                    }
+                }
+
+                element = Console.ReadLine();
+            }
+
+            trainers = trainers.OrderByDescending(t => t.Value.Badges).ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var trainer in trainers)
+            {
+                Console.WriteLine($"{trainer.Value.Name} {trainer.Value.Badges} {trainer.Value.Pokemons.Count}");
+            }
         }
     }
 }
---
+++ Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, List<Pokemon> pokemons)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int? Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+    }
+}
